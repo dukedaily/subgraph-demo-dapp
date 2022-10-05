d25d7fcd5d87bfb15feef5d76beccfa6abba81aa
duke.du <duke.du@bybit.com>
2022-10-05 13:20:42 +0800

    Updates

---
+++ precache-manifest.b42779a37a6384af9ad2f4e33495caa4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9e06044b593613a1f68b",
+    "url": "/subgraph-demo-dapp/static/css/main.b79557a5.chunk.css"
+  },
+  {
+    "revision": "9e06044b593613a1f68b",
+    "url": "/subgraph-demo-dapp/static/js/main.9e06044b.chunk.js"
+  },
+  {
+    "revision": "b2c9b6f876dcb2191c5a",
+    "url": "/subgraph-demo-dapp/static/js/runtime~main.b2c9b6f8.js"
+  },
+  {
+    "revision": "bcbcf9b1940887d1cbbf",
+    "url": "/subgraph-demo-dapp/static/js/2.bcbcf9b1.chunk.js"
+  },
+  {
+    "revision": "b522a64fdd45873f45d92449b84af99d",
+    "url": "/subgraph-demo-dapp/index.html"
+  }
+];
